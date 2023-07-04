f895500d3556cb967b98066b0aa0b642dfc4568e
kigen chesire <ekchesire10@gmail.com>
2023-07-04 18:57:15 +0300

    additional files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    packagingOptions {
+        resources.excludes.add("META-INF/*")
+
+    }
 }
 
 dependencies {
@@ -38,10 +42,20 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation ("ca.uhn.hapi.fhir:hapi-fhir-client:5.4.0")
 
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    implementation("com.google.android.fhir:engine:0.1.0-beta03")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
+
+
 
 
     // 3 Add dependencies for Structured Data Capture Library and Fragment KTX
--- app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
+++ app/src/main/java/com/google/codelab/sdclibrary/MainActivity.kt
@@ -16,27 +16,32 @@
 
 package com.google.codelab.sdclibrary
 
-import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import ca.uhn.fhir.context.FhirContext
+import ca.uhn.fhir.context.FhirVersionEnum
 import com.google.android.fhir.datacapture.QuestionnaireFragment
+import com.google.android.fhir.datacapture.mapping.ResourceMapper
+import kotlinx.coroutines.launch
+import org.hl7.fhir.r4.model.Questionnaire
+import org.hl7.fhir.r4.model.QuestionnaireResponse
+
 
 class MainActivity : AppCompatActivity() {
 
   var questionnaireJsonString: String? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(savedInstanceState: android.os.Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     // 4.2 Replace with code from the codelab to add a questionnaire fragment.
     // Step 2: Configure a QuestionnaireFragment
-    questionnaireJsonString = getStringFromAssets("questionnaire.json")
+    questionnaireJsonString = getStringFromAssets("records-final.R4.json")
 
     if (savedInstanceState == null) {
       supportFragmentManager.commit {
@@ -52,8 +57,61 @@ class MainActivity : AppCompatActivity() {
   private fun submitQuestionnaire() {
 
     // 5 Replace with code from the codelab to get a questionnaire response.
+    // Get a questionnaire response
+    val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view)
+            as QuestionnaireFragment
+    val questionnaireResponse = fragment.getQuestionnaireResponse()
+
+// Print the response to the log
+    val jsonParser = FhirContext.forCached(FhirVersionEnum.R4).newJsonParser()
+    val questionnaireResponseString =
+      jsonParser.encodeResourceToString(questionnaireResponse)
+    Log.d("response", questionnaireResponseString)
 
     // 6 Replace with code from the codelab to extract FHIR resources from QuestionnaireResponse.
+    lifecycleScope.launch {
+      val questionnaire =
+        jsonParser.parseResource(questionnaireJsonString) as Questionnaire
+      val bundle = ResourceMapper.extract(questionnaire, questionnaireResponse)
+      Log.d("extraction result", jsonParser.encodeResourceToString(bundle))
+      val fhirServerUrl = "http://hapi.fhir.org/baseR4/" // Replace with your FHIR server URL
+      FHIRUploader.uploadQuestionnaireResponse(fhirServerUrl, questionnaireResponseString)
+    }
+
+  }
+
+  object FHIRUploader {
+    fun uploadQuestionnaireResponse(fhirServerUrl: String, questionnaireResponseString: String) {
+      // Create a FHIR context
+      val ctx = FhirContext.forR4()
+
+      // Create a FHIR client
+      val client = ctx.newRestfulGenericClient(fhirServerUrl)
+
+      // Parse the QuestionnaireResponse from the JSON string
+      val questionnaireResponse = ctx.newJsonParser()
+        .parseResource(QuestionnaireResponse::class.java, questionnaireResponseString)
+
+      // Create a Bundle to contain the QuestionnaireResponse
+      val bundle = org.hl7.fhir.r4.model.Bundle()
+      bundle.type = org.hl7.fhir.r4.model.Bundle.BundleType.TRANSACTION
+      bundle.addEntry().setResource(questionnaireResponse).request.method = org.hl7.fhir.r4.model.Bundle.HTTPVerb.PUT
+
+      // Upload the Bundle to the FHIR server
+      val responseBundle = client.transaction().withBundle(bundle).execute()
+
+      // Process the server response
+      for (entry in responseBundle.entry) {
+        if (entry.response.status.startsWith("20")) {
+          val resourceId = entry.response.location
+          println("Successfully uploaded QuestionnaireResponse with ID: ${resourceId}")
+        } else {
+          println("Failed to upload QuestionnaireResponse. Response status: ${entry.response.status}")
+        }
+      }
+    }
+
+
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
